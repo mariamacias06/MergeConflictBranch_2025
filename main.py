1cc8ec8854e490418ddd844051d84531fa7f13c7
María Macías Delgado <merimacias@MacBook-Air-de-Maria-Macias.local>
2025-04-15 18:36:42 +0200

    first commit studentA

---
+++ main.py
@@ -0,0 +1,12 @@
+import utilitiesA as utilA
+
+def main():
+    a = 7
+    b = 5
+
+    c = utilA.SumaA(a,b)
+
+    print(f"El valor de la suma de a y b es: {c}")
+
+if __name__ == "__main__":
+    main()
---
+++ utilitiesA.py
@@ -0,0 +1,4 @@
+
+def SumaA(a:int,b:int):
+    c=a+b
+    return c
